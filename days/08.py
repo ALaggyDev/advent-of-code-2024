fda8bc1729ee88b5563dfea98c474337fbe8b35d
ALaggyDev <renlin456123@gmail.com>
2024-12-08 13:32:47 +0800

    Day 8

---
+++ days/08.py
@@ -0,0 +1,85 @@
+import itertools
+import aoc
+
+day = 8
+aoc_input = aoc.read_input(day)
+aoc_example = aoc.read_example(day)
+
+# ----------
+
+
+def part_1(input: str):
+    lines = input.splitlines()
+
+    antennas_freq: dict[str, list[tuple[int, int]]] = {}
+
+    # Find all antennas
+    for (y, line) in enumerate(lines):
+        for (x, char) in enumerate(list(line)):
+            if char == ".":
+                continue
+
+            if char not in antennas_freq:
+                antennas_freq[char] = []
+            antennas_freq[char].append((x, y))
+
+    antinodes = set()
+
+    # Loop through all pairs of antennas
+    for antennas in antennas_freq.values():
+        for (ant_1, ant_2) in itertools.permutations(antennas, 2):
+            pos = (2 * ant_1[0] - ant_2[0], 2 * ant_1[1] - ant_2[1])
+
+            if 0 <= pos[0] < len(lines) and 0 <= pos[1] < len(lines[0]):
+                antinodes.add(pos)
+
+    return len(antinodes)
+
+
+def part_2(input: str):
+    lines = input.splitlines()
+
+    antennas_freq: dict[str, list[tuple[int, int]]] = {}
+
+    # Find all antennas
+    for (y, line) in enumerate(lines):
+        for (x, char) in enumerate(list(line)):
+            if char == ".":
+                continue
+
+            if char not in antennas_freq:
+                antennas_freq[char] = []
+            antennas_freq[char].append((x, y))
+
+    antinodes = set()
+
+    # Loop through all pairs of antennas
+    for antennas in antennas_freq.values():
+        for (ant_1, ant_2) in itertools.permutations(antennas, 2):
+            antinodes.add(ant_1)
+
+            for i in itertools.count(1):
+                pos = (i * (ant_1[0] - ant_2[0]) + ant_1[0],
+                       i * (ant_1[1] - ant_2[1]) + ant_1[1])
+
+                if 0 <= pos[0] < len(lines) and 0 <= pos[1] < len(lines[0]):
+                    antinodes.add(pos)
+                else:
+                    break
+
+    return len(antinodes)
+
+# ----------
+
+
+use_example = False
+
+if use_example:
+    print(f"----- Day {day:02} ----- (Example Input)")
+    input = aoc_example
+else:
+    print(f"----- Day {day:02} ----- (Actual Input)")
+    input = aoc_input
+
+aoc.run(lambda: part_1(input), part=1)
+aoc.run(lambda: part_2(input), part=2)
